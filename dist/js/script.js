19ded2bec32358c1459472b8f9896ab98b7c70a8
Shaualiev Nurbolat <shaualiev.ns@gmail.com>
2021-02-22 20:52:49 +0600

    'phpMailer'

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,3 +17,25 @@ counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+const link = document.querySelectorAll('.menu__link');
+
+link.forEach( (item) => {
+	item.addEventListener('click', () => {
+		menu.classList.remove('active');
+	})
+});
+
+$('form').submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		type: 'POST',
+		url: 'mailer/smart.php',
+		data: $(this).serailize()
+	}).done(function() {
+		$(this).find('input').val('');
+
+		$('form').trigger('reset');
+	});
+	return false;
+});
+
